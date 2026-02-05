dd31631e60e3b783454cf4533ef465f497d0bf1c
akakporichard135-source <akakporichard135@gmail.com>
2026-02-05 18:05:27 +0000

    Create analysis.py

---
+++ analysis.py
@@ -0,0 +1,6 @@
+# Simple data analysis example
+sales = [120, 340, 560, 220, 150]
+total_sales = sum(sales)
+average_sales = total_sales / len(sales)
+print("Total sales:", total_sales)
+print("Average sales:", average_sales)
